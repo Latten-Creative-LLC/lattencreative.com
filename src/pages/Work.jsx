03bfe08bdcaa58799d6f272a5c77ed7c83117f5b
James Latten <hello@jameslatten.com>
2023-02-28 12:25:05 -0500

    Link fixes

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -28,58 +28,64 @@ export default function Work() {
                 <div className='work-page-main-row row'>
                   <div className='work-page-main-left col-lg-4'>
                     <div className='work-page-linear-case case-one' style={{backgroundImage: "url('/images/case_studies/st_pierre/cover_image.webp')"}}>
-                      <div className='work-page-linear-case-inner'>
-                        <div className='top-category'>
-                          <ul>
-                            <li>Development</li>
-                            <li>UI/UX</li>
-                            <li>Branding</li>
-                          </ul>
-                        </div>
+                      <a href='https://saintpierre.world/'>
+                        <div className='work-page-linear-case-inner'>
+                          <div className='top-category'>
+                            <ul>
+                              <li>Development</li>
+                              <li>UI/UX</li>
+                              <li>Branding</li>
+                            </ul>
+                          </div>
 
-                        <div className='bottom-title'>
-                          <h2>St. Pierre</h2>
-                          <p>An underground Ohio fashion brand</p>
-                        </div>
-                      </div>  
+                          <div className='bottom-title'>
+                            <h2>St. Pierre</h2>
+                            <p>An underground Ohio fashion brand</p>
+                          </div>
+                        </div>  
+                      </a>
                     </div>
                   </div>
                   <div className='work-page-main-right col-lg-8'>
                   <div className='work-page-horizontal-case case-one' style={{backgroundImage: "url('/images/case_studies/jameslatten/cover_image.png')"}}>
-                      <div className='work-page-horizontal-case-inner'>
-                        <div className='top-category'>
-                          <Marquee gradient={false} speed={100}>
-                            <h3 className='featured'>Featured Project</h3>
-                          </Marquee>
+                      <a href='https://jameslatten.com/'>
+                        <div className='work-page-horizontal-case-inner'>
+                          <div className='top-category'>
+                            <Marquee gradient={false} speed={100}>
+                              <h3 className='featured'>Featured Project</h3>
+                            </Marquee>
 
-                          <ul>
-                            <li>Development</li>
-                            <li>UI/UX</li>
-                          </ul>
-                        </div>
+                            <ul>
+                              <li>Development</li>
+                              <li>UI/UX</li>
+                            </ul>
+                          </div>
 
-                        <div className='bottom-title'>
-                          <h2>James Latten</h2>
-                          <p>Portfolio website for James Latten</p>
+                          <div className='bottom-title'>
+                            <h2>James Latten</h2>
+                            <p>Portfolio website for James Latten</p>
+                          </div>
                         </div>
-                      </div>  
+                      </a>  
                     </div>
 
                     <div className='work-page-horizontal-case case-one' style={{backgroundImage: "url('https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"}}>
-                      <div className='work-page-horizontal-case-inner'>
-                        <div className='top-category'>
-                          <ul>
-                            <li>Development</li>
-                            <li>UI/UX</li>
-                            <li>Branding</li>
-                          </ul>
-                        </div>
+                      <a href='https://lattentechnologies.com/'>
+                        <div className='work-page-horizontal-case-inner'>
+                          <div className='top-category'>
+                            <ul>
+                              <li>Development</li>
+                              <li>UI/UX</li>
+                              <li>Branding</li>
+                            </ul>
+                          </div>
 
-                        <div className='bottom-title'>
-                          <h2>Latten Technologies, LLC</h2>
-                          <p>An underground Ohio fashion brand</p>
+                          <div className='bottom-title'>
+                            <h2>Latten Technologies, LLC</h2>
+                            <p>An underground Ohio fashion brand</p>
+                          </div>
                         </div>
-                      </div>  
+                      </a>  
                     </div>
                   </div>
                 </div>
